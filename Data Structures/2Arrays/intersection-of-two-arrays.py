e9f94c09d2d508586316eb913a49b1104c1a3363
Rizal Ilham <rzlilhm09@gmail.com>
2024-03-10 23:07:00 +0700

    349. Intersection of Two Arrays

--- Data Structures/2Arrays/intersection-of-two-arrays.py
+++ Data Structures/2Arrays/intersection-of-two-arrays.py
@@ -1,3 +1,5 @@
+# Problem Link: https://leetcode.com/problems/intersection-of-two-arrays/description/
+
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         resp = []
