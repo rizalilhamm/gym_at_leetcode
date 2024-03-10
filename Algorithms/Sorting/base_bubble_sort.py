aedd9163323e48cd36fc09a6ce09886aef1eb57e
Rizal Ilham <rzlilhm09@gmail.com>
2024-03-10 23:11:19 +0700

    [ALGO] Base bubble_sort in python

---
+++ Algorithms/Sorting/base_bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+            # Traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
